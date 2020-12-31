a5e9c78df35366705acfd12a86b2749f99ecab11
Michael M <m.d.mcnamara@gmail.com>
2020-12-31 10:31:16 -0500

    Changed variable names in startup blink script.

--- startupBlink.py
+++ startupBlink.py
@@ -2,21 +2,24 @@
 
 from blinkt import set_pixel, set_brightness, show, clear
 import time, colorsys
+import EasyBlinkt
+import logging
 
+logging.basicConfig(level = logging.DEBUG)
 
-amplitude = 50
-spacing = 360.0 / 16.0
-hue = 0
-blink_duration = 5
+AMPLITUDE = 50
+SPACING = 360.0 / 16.0
+HUE = 0
+BLINK_DURATION = 5
 
 
 def do_one_rainbow():
-    hue = int(time.time() * 100) % 360
+    HUE = int(time.time() * 100) % 360
 
     for x in range(8):
-        offset = x * spacing
-        h = ((hue + offset) % 360) / 360.0
-        r, g, b = [int(c * amplitude) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+        offset = x * SPACING
+        h = ((HUE + offset) % 360) / 360.0
+        r, g, b = [int(c * AMPLITUDE) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
         set_pixel(x, r, g, b)
 
     show()
@@ -25,13 +28,20 @@ def do_one_rainbow():
 
 
 set_brightness(0.1)
-
 start_time = time.time()
 continue_blinking = True
 
 while continue_blinking:
     do_one_rainbow()
-
     current_duration = time.time() - start_time
-    if (current_duration > blink_duration):
+    if (current_duration > BLINK_DURATION):
         continue_blinking = False
+
+for x in range(8):
+    EasyBlinkt.setBlinktLight(
+        light_number = x
+        ,brightness_level = 0.1
+        ,color = [54, 43, 11]
+        ,duration = 1
+        ,gradual = True
+    )
