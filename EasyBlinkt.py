c96cd970232aeecebe2eb02bf48abd361f64739f
Michael M <m.d.mcnamara@gmail.com>
2020-12-31 10:32:09 -0500

    Made EasyBlinkt script correctly wait while setting gradual light.

--- EasyBlinkt.py
+++ EasyBlinkt.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 import logging
+import os
 from sys import argv
 from time import sleep
 from blinkt import set_pixel, set_brightness, show, clear
 
+LIGHT_IN_USE_VAR="BLINKT_LIGHTS_IN_USE"
 STEP_DICT = {
     "short": {
         "steps": 100
@@ -15,6 +17,14 @@ STEP_DICT = {
         ,"multiplier": 0.1
     }
 }
+
+'''
+def (light_number: int):
+    if (not os.environ.get(LIGHT_IN_USE_VAR))
+    lights_in_use = [int(light) for light in os.environ['BLINKT_LIGHTS_IN_USE'].split('|')]
+    
+    if light_number in lights_in_use:
+'''
 
 def setBlinktLight(light_number: int, brightness_level: float, color: list, duration: int = 1, gradual: bool = False):
     logging.debug(f'''
@@ -27,29 +37,32 @@ def setBlinktLight(light_number: int, brightness_level: float, color: list, dura
 
     logging.info('Setting light...')
 
-    if (gradual):
-        clear()
-        set_pixel(light_number, color[0], color[1], color[2], 0.0)
-        show()
+    try:
+        if (gradual):
+            # Create switch for number of steps here if duration > 10
 
-        # Create switch for number of steps here if duration > 10
+            number_of_steps = int(100 * brightness_level)
+            interval = duration / number_of_steps
+            logging.info(f'Interval set to {interval} seconds.')
 
-        number_of_steps = int(100 * brightness_level)
-        interval = duration / number_of_steps
-        logging.info(f'Interval set to {interval} seconds.')
-
-        for x in range(1, number_of_steps):
-            x = round(x * 0.01, duration)
-            logging.debug(f'Setting brightness to {x}')
-            set_brightness(x)
+            for x in range(1, number_of_steps):
+                x = round(x * 0.01, duration)
+                logging.debug(f'Setting brightness to {x}')
+                set_pixel(light_number, color[0], color[1], color[2], x)
+                show()
+                logging.info(f'Sleeping for {interval} seconds')
+                sleep(interval)
+        else:
+            set_pixel(light_number, color[0], color[1], color[2], brightness=brightness_level)
             show()
-            # sleep(interval)
-    else:
-        set_pixel(light_number, color[0], color[1], color[2], brightness=brightness_level)
-        show()
 
-        logging.info(f'Sleeping for {duration} seconds.')
-        sleep(duration)
+            logging.info(f'Sleeping for {duration} seconds.')
+            sleep(duration)
+    except:
+        logging.exception(f'Exception while trying to set light!')
+
+    logging.info('Clearing light...')
+    set_pixel(light_number, 0, 0, 0, 0)
 
 
 if __name__ == '__main__':
